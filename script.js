77784724da22db4a6c64717af121811c3b24da4f
Zequez <zequez@gmail.com>
2023-04-22 15:21:47 -0300

    Disable Waveform render for now

--- script.js
+++ script.js
@@ -35,6 +35,7 @@
   }
   renderAudioFiles();
 
+  const $waveform = $$("waveform");
   const $prevButton = $$("prev-button");
   const $playButton = $$("play-button");
   const $playIcon = $$("play-icon");
@@ -57,6 +58,14 @@
   let after = null;
   let current = null;
   let fileIndex = 0;
+
+  loadFile(0);
+
+  setInterval(() => {
+    if (current && !current.paused) {
+      $timeCurrent.innerText = formatDuration(current.currentTime);
+    }
+  }, 100);
 
   function loadFile(index, play) {
     if (index < 0) {
@@ -104,6 +113,7 @@
       }
     }
 
+    // renderWaveform(current);
     setTotalTime();
   }
 
@@ -132,12 +142,6 @@
     }
   }
 
-  setInterval(() => {
-    if (current && !current.paused) {
-      $timeCurrent.innerText = formatDuration(current.currentTime);
-    }
-  }, 100);
-
   function formatDuration(duration) {
     let minutes = Math.floor(duration / 60);
     let seconds = Math.floor(duration % 60);
@@ -150,5 +154,72 @@
     return formattedDuration;
   }
 
-  loadFile(0);
+  // function renderWaveform(audioObject) {
+  //   let audioContext = new AudioContext();
+
+  //   // load the audio file and decode it into a buffer
+  //   let fileReader = new FileReader();
+  //   fileReader.onload = function () {
+  //     audioContext.decodeAudioData(fileReader.result, function (buffer) {
+  //       // create an AnalyserNode to get frequency and time-domain data
+  //       let analyser = audioContext.createAnalyser();
+  //       analyser.fftSize = 2048;
+
+  //       // connect the buffer to the analyser
+  //       let audioBufferSource = audioContext.createBufferSource();
+  //       audioBufferSource.buffer = buffer;
+  //       audioBufferSource.connect(analyser);
+  //       audioBufferSource.start(0);
+
+  //       // render the waveform using a canvas element
+  //       let canvasContext = $waveform.getContext("2d");
+  //       let canvasWidth = $waveform.width;
+  //       let canvasHeight = $waveform.height;
+  //       let bufferLength = analyser.frequencyBinCount;
+  //       let dataArray = new Uint8Array(bufferLength);
+  //       let pixelSpacing = 1;
+  //       let scale = canvasHeight / 2;
+
+  //       canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+  //       canvasContext.beginPath();
+  //       canvasContext.strokeStyle = "rgba(0, 0, 0, 0.5)";
+  //       canvasContext.lineWidth = 1;
+
+  //       function renderFrame() {
+  //         // get the time-domain data and calculate the waveform
+  //         analyser.getByteTimeDomainData(dataArray);
+
+  //         for (let i = 0; i < canvasWidth; i++) {
+  //           let sampleIndex = Math.floor((i * bufferLength) / canvasWidth);
+  //           let sampleValue = dataArray[sampleIndex] / 128 - 1;
+  //           let x = i * pixelSpacing;
+  //           let y = (1 + sampleValue) * scale;
+
+  //           if (i === 0) {
+  //             canvasContext.moveTo(x, y);
+  //           } else {
+  //             canvasContext.lineTo(x, y);
+  //           }
+  //         }
+
+  //         canvasContext.stroke();
+  //       }
+
+  //       // start rendering the waveform
+  //       renderFrame();
+  //     });
+  //   };
+
+  //   let xhr = new XMLHttpRequest();
+  //   xhr.open("GET", audioObject.src, true);
+  //   xhr.responseType = "blob";
+  //   xhr.onload = function () {
+  //     if (this.status === 200) {
+  //       // create a blob URL from the blob response
+  //       let blobUrl = URL.createObjectURL(this.response);
+  //       fileReader.readAsArrayBuffer(blobUrl);
+  //     }
+  //   };
+  //   xhr.send();
+  // }
 })();
