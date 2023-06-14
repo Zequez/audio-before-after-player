82e355af56c54527787c54308898262918d4c427
Zequez <zequez@gmail.com>
2023-06-14 13:45:13 -0300

    Embeddable page

---
+++ src/app/(embedded)/embed/[userId]/[playlistIndex]/page.tsx
@@ -0,0 +1,62 @@
+// import Player from "@/app/components/Player";
+// import Layout from "@/app/layout";
+import { notFound } from "next/navigation";
+import { createClient } from "@supabase/supabase-js";
+import Player from "@/components/Player";
+
+if (
+  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+) {
+  throw new Error(
+    "Environment variables NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY must be available"
+  );
+}
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export const metadata = {
+  title: "Before After Audio Player",
+  description: "A simple before after audio player",
+};
+
+const EmbedPage = async ({
+  params,
+}: {
+  params: { userId: string; playlistIndex: string };
+}) => {
+  const { data, error } = await supabase
+    .from("docs")
+    .select("*")
+    .eq("context", "soundtoggle")
+    .eq("ownerId", params.userId);
+
+  if (error) {
+    throw new Error(
+      `Error connecting to the database. Error code: ${error.code}`
+    );
+    // return (
+    //   <div>
+    //     throwf
+    //     return <ErrorPage statusCode={500} />
+    //   </div>
+    // );
+  }
+
+  const index = parseInt(params.playlistIndex);
+
+  if (
+    !data.length ||
+    !data[0]?.doc?.playlists ||
+    !data[0].doc.playlists[index]
+  ) {
+    notFound();
+  }
+
+  return <Player playlist={data[0].doc.playlists[index]} />;
+};
+
+export default EmbedPage;
