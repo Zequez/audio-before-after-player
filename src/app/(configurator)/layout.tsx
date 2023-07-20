86bd0714c350363e8b57a05ae04ee4ed23e977ba
Zequez <zequez@gmail.com>
2023-07-20 10:53:32 -0300

    Updated node maybe fixed thing

--- src/app/(configurator)/layout.tsx
+++ src/app/(configurator)/layout.tsx
@@ -1,7 +1,5 @@
 import "@/globals.css";
 import { Inter } from "next/font/google";
-import Image from "next/image";
-import logo from "@/logo.svg";
 import AuthProvider from "@/lib/AuthProvider";
 import Header from "@/components/Header";
 
